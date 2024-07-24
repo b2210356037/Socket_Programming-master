ad47cb921eaf8f54e3d31c945fbc0de942212610
İclal Gül <iclalgulll67@gmail.com>
2024-07-24 10:18:29 +0300

    comment_update

--- Socket_Programming-master/main.py
+++ Socket_Programming-master/main.py
@@ -3,8 +3,7 @@ import socket
 import threading
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import QLocale
-from interface_ui import Ui_MainWindow  # PyQt5 GUI dosyanızın adı
-
+from interface_ui import Ui_MainWindow  # Your PyQt5 GUI file name
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -14,74 +13,71 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setupConnections()
         self.initializeDateTime()
 
-
-        # Takvim widget'ını İngilizceye ayarlama
+        # Set the calendar widget to English
         self.ui.calendarWidget.setLocale(QLocale(QLocale.English, QLocale.UnitedStates))
     
-
-        # Tcp bağlantısı başlatma
+        # Start TCP connection
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect(('192.168.77.10', 12345))
 
         self.host_ip = self.client_socket.getsockname()[0]
         host_ip = "192.168.77.10"
-        # Mesajları almak için iş parçacığını başlat
+        # Start a thread to receive messages
         self.receive_thread = threading.Thread(target=self.receive_messages)
         self.receive_thread.daemon = True
         self.receive_thread.start()
 
-        # Zamanlayıcı oluştur ve her 1000 ms (1 saniye) aralıklarla updateDateTime metodunu çağır
+        # Create a timer and call updateDateTime method every 1000 ms (1 second)
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.updateDateTime)
-        self.timer.start(1000)  # 1 saniyede bir
+        self.timer.start(1000)  # Every 1 second
 
     def setupConnections(self):
-        # Button'lara tıklama olaylarını bağlıyoruz
+        # Connect button clicks to their respective handlers
         self.ui.pushButton.clicked.connect(self.handlePushButtonClick)
-        # Calendar widget'ın tıklama sinyalini show_date metoduna bağlıyoruz
+        # Connect the calendar widget's clicked signal to show_date method
         self.ui.calendarWidget.clicked.connect(self.show_date)
         
 
     def initializeDateTime(self):
-        # Mevcut tarihi ve saati al ve widget'lara ata
+        # Get the current date and time and set them to the widgets
         current_time = QtCore.QTime.currentTime()
         current_date = QtCore.QDate.currentDate()
 
-        # Widget'lara tarih ve saat atama
+        # Set date and time to widgets
         self.ui.timeEdit.setTime(current_time)
         self.ui.dateEdit.setDate(current_date)
 
     def updateDateTime(self):
-        # Mevcut tarihi ve saati al ve widget'lara ata
+        # Get the current date and time and set them to the widgets
         current_time = QtCore.QTime.currentTime()
         current_date = QtCore.QDate.currentDate()
 
-        # Widget'lara tarih ve saat atama
+        # Set date and time to widgets
         self.ui.timeEdit.setTime(current_time)
         self.ui.dateEdit.setDate(current_date)
 
     def show_date(self, date):
-    # Tarihi QDate tipinde alıp İngilizce formatında bir mesaj kutusunda gösterme
-     locale = QLocale(QLocale.English, QLocale.UnitedStates)
-     formatted_date = locale.toString(date, QLocale.LongFormat)
-     QtWidgets.QMessageBox.information(self, "Selected Date", formatted_date)
-
+        # Display the date in English format in a message box
+        locale = QLocale(QLocale.English, QLocale.UnitedStates)
+        formatted_date = locale.toString(date, QLocale.LongFormat)
+        QtWidgets.QMessageBox.information(self, "Selected Date", formatted_date)
 
     def handlePushButtonClick(self):
-        # Kullanıcıdan gelen mesajı al
+        # Get the message from the user
         user_message = self.ui.lineEdit_2.text()
 
-        # Mesajı textEdit widget'ına ekle
+        # Add the message to the textEdit widget
         self.ui.textEdit.append(f"{self.host_ip}: {user_message}")
 
-        # Mesajı TCP sunucusuna gönder
+        # Send the message to the TCP server
         self.client_socket.sendall(f"{self.host_ip}: {user_message}".encode())
 
-        # LineEdit widget'ını temizle
+        # Clear the lineEdit widget
         self.ui.lineEdit_2.clear()
 
     def handlePushButton2Click(self):
-        # İkinci düğmeye tıklanmasıyla bu fonksiyon çalışır
+        # This function is called when the second button is clicked
         QtWidgets.QMessageBox.information(self, "Button Clicked", "Cancel Button Clicked!")
 
     def receive_messages(self):
@@ -89,24 +85,16 @@ class MainWindow(QtWidgets.QMainWindow):
             try:
                 message = self.client_socket.recv(1024).decode()
                 if message:
-                    # Mesajı textEdit widget'ına ekle
-                    #if the sender has the server ip, display the message in red
+                    # Add the message to the textEdit widget
+                    # If the sender has the server IP, display the message in red
                     if(self.host_ip == self.client_socket.getsockname()[0]):
                         self.ui.textEdit.append(f"<font color='red'> Server: {message}</font>")
-
-
-                    # if message.startswith("Server:"):
-                    #     self.ui.textEdit.append(f"<font color='red'>{message}</font>")
                     else:
                         self.ui.textEdit.append(message)
-                    #self.ui.textEdit.append(message)
                 else:
                     break
             except:
                 break
-    
-          
-            
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
